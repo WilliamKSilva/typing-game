c167f6d0b7d56d1a50e46e19c8aa29857a7feef4
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-01-25 17:51:30 -0300

    feat(web): add basic home page

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  encapsulation: ViewEncapsulation.None,
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
@@ -10,4 +11,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'web';
+
+  ngOnInit() {
+    this.teste()
+  }
+
+  teste() {
+    console.log("aiushdsuia")
+  }
 }
